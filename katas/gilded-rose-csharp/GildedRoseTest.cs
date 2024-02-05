7386e5443a7cbe8c17453136cd09afd21e2a9696
tjhleeds <tjhleeds@users.noreply.github.com>
2024-02-05 20:22:29 +0000

    test: sell in decreases each day

--- katas/gilded-rose-csharp/GildedRoseTest.cs
+++ katas/gilded-rose-csharp/GildedRoseTest.cs
@@ -7,12 +7,33 @@ namespace csharp
     public class GildedRoseTest
     {
         [Test]
-        public void foo()
+        public void SingleDay_SellInDecreasesByOne()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 10, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
+
             app.UpdateQuality();
-            Assert.AreEqual("foo", Items[0].Name);
+
+            Assert.AreEqual(9, Items[0].SellIn);
+        }
+
+
+        [Test]
+        [TestCase(3)]
+        [TestCase(7)]
+        [TestCase(20)]
+        public void MultipleDays_SellInDecreasesByNumberOfDays(int numberOfDays)
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 30, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+
+            for (var i = 0; i < numberOfDays; i++)
+            {
+                app.UpdateQuality();
+            }
+
+            var expectedSellIn = 30 - numberOfDays;
+            Assert.AreEqual(expectedSellIn, Items[0].SellIn);
         }
     }
 }
