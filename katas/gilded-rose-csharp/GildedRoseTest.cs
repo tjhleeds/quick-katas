ce6b782cb9b39bad8cb4dcc742826b4fe1251a48
tjhleeds <tjhleeds@users.noreply.github.com>
2024-02-05 20:25:09 +0000

    test: sell in goes negative

--- katas/gilded-rose-csharp/GildedRoseTest.cs
+++ katas/gilded-rose-csharp/GildedRoseTest.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 
 namespace csharp
 {
@@ -34,6 +35,31 @@ namespace csharp
 
             var expectedSellIn = 30 - numberOfDays;
             Assert.AreEqual(expectedSellIn, Items[0].SellIn);
+        }
+
+        [Test]
+        public void SellInStartsAtZero_SellInGoesNegative()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+
+            app.UpdateQuality();
+
+            Assert.AreEqual(-1, Items[0].SellIn);
+        }
+
+        [Test]
+        [TestCase(-5)]
+        [TestCase(-15)]
+        [TestCase(-29)]
+        public void SellInStartsNegative_SellInContinuesToGoDown(int initialSellIn)
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = initialSellIn, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+
+            app.UpdateQuality();
+
+            Assert.AreEqual(initialSellIn - 1, Items[0].SellIn);
         }
     }
 }
