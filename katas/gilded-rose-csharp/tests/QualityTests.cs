f18ae857b2aa96d9174f689284a95f785a457b87
tjhleeds <tjhleeds@users.noreply.github.com>
2024-12-04 13:10:13 +0000

    test: Once the sell by date has passed, Quality degrades twice as fast

--- katas/gilded-rose-csharp/tests/QualityTests.cs
+++ katas/gilded-rose-csharp/tests/QualityTests.cs
@@ -9,8 +9,26 @@ public class QualityTests {
     [TestCase(10, 1, 9)]
     [TestCase(10, 7, 3)]
     [TestCase(3, 6, -3)]
-    public void DecreaseEachDay(int startQuality, int days, int expectedQuality){
+    public void SellInPositive_DecreaseByOneEachDay(int startQuality, int days, int expectedQuality)
+    {
         IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 10, Quality = startQuality } };
+        GildedRose app = new GildedRose(Items);
+
+        for(int i = 0;  i < days; i++){
+            app.UpdateQuality();
+        }
+
+        Assert.AreEqual(expectedQuality, Items[0].Quality);
+    }
+
+    [Theory]
+    [TestCase(1, 10, 1, 9)]
+    [TestCase(0, 10, 7, 3)]
+    [TestCase(-1, 0, 6, -12)]
+    [TestCase(-10, 1, 6, -10)]
+    public void SellInNegative_DecreaseByTwoEachDay(int sellIn, int startQuality, int days, int expectedQuality)
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = sellIn, Quality = startQuality } };
         GildedRose app = new GildedRose(Items);
 
         for(int i = 0;  i < days; i++){
